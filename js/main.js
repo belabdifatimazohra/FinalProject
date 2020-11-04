92388d905ee70d53e9d12424d98efffe852a545f
belabdifatimazohra <fatima.zohra.beabdi@gmail.com>
2020-11-04 21:48:32 +0100

    Accueil animation background image

--- js/main.js
+++ js/main.js
@@ -30,11 +30,27 @@ function checkLog() {
   }
 }
 
-$(function () {
-  $("#datepicker")
-    .datepicker({
-      autoclose: true,
-      todayHighlight: true,
-    })
-    .datepicker("update", new Date());
+
+
+
+// Animation accueil
+var bgImageArray = ["1", "2", "3", "4"],
+base = "../res/Accueil/bg",
+secs = 4;
+bgImageArray.forEach(function(img){
+    new Image().src = base + img; 
+    // caches images, avoiding white flash between background replacements
 });
+
+function backgroundSequence() {
+    window.clearTimeout();
+    var k = 0;
+    for (i = 0; i < bgImageArray.length; i++) {
+        setTimeout(function(){ 
+            document.getElementById('animated-bg').style.background = "url(" + base + bgImageArray[k] +".jpeg" +") no-repeat right center";
+            document.getElementById('animated-bg').style.backgroundSize ="contain";
+        if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }            
+        }, (secs * 1000) * i)   
+    }
+}
+backgroundSequence();
