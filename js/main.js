c9b8c02fe372c1f273f2bbb33c1655def339b5e7
belabdifatimazohra <fatima.zohra.belabdi@gmail.com>
2020-11-05 16:12:18 +0100

    changer les images background accuiel à 6 & commencer section rencontre moi

--- js/main.js
+++ js/main.js
@@ -34,23 +34,23 @@ function checkLog() {
 
 
 // Animation accueil
-var bgImageArray = ["1", "2", "3", "4"],
-base = "../res/Accueil/bg",
-secs = 4;
-bgImageArray.forEach(function(img){
-    new Image().src = base + img; 
-    // caches images, avoiding white flash between background replacements
+var bgImageArray = ["1", "2", "3", "4", "5", "6"],
+  base = "../res/Accueil/bg",
+  secs = 4;
+bgImageArray.forEach(function (img) {
+  new Image().src = base + img;
+  // caches images, avoiding white flash between background replacements
 });
 
 function backgroundSequence() {
-    window.clearTimeout();
-    var k = 0;
-    for (i = 0; i < bgImageArray.length; i++) {
-        setTimeout(function(){ 
-            document.getElementById('animated-bg').style.background = "url(" + base + bgImageArray[k] +".jpeg" +") no-repeat right center";
-            document.getElementById('animated-bg').style.backgroundSize ="contain";
-        if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }            
-        }, (secs * 1000) * i)   
-    }
+  window.clearTimeout();
+  var k = 0;
+  for (i = 0; i < bgImageArray.length; i++) {
+    setTimeout(function () {
+      document.getElementById('animated-bg').style.background = "url(" + base + bgImageArray[k] + ".jpg" + ") no-repeat ";
+      document.getElementById('animated-bg').style.backgroundSize = "100% 100%";
+      if ((k + 1) === bgImageArray.length) { setTimeout(function () { backgroundSequence() }, (secs * 1000)) } else { k++; }
+    }, (secs * 1000) * i)
+  }
 }
 backgroundSequence();
