e1a50eb65f93f63ca19968849e0c858ef0bdc928
belabdifatimazohra <fatima.zohra.beabdi@gmail.com>
2020-11-19 21:44:25 +0100

    Add function calcul IMC

--- js/login.js
+++ js/login.js
@@ -4,26 +4,19 @@ let mail = document.querySelector(".email");
 let password = document.querySelector(".password");
 let btnLog = document.querySelector(".submit-btn"); //Sumbit button
 
-//2. addEventListener and check email & password using function checkLog
-btnLog.addEventListener("click", checkLog);
-
 //3. Ecrire la fonction de controle
 
-function checkLog() {
+const checkLog = () => {
   let displayIn = document.querySelector(".loged-in");
   let displayErr = document.querySelector(".err");
-  console.log("Email: " + mail.value);
-  console.log("Password: " + password.value);
-  console.log(displayIn);
-
-  var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (mail.value.match(mailformat)) {
-    console.log(displayIn);
     document.querySelector(".content").style.display = "none";
-
     displayIn.style.display = "block";
   } else {
-    //alert("Email or password incorrect !!");
     displayErr.innerHTML = "Email or password incorrect !!";
   }
-}
+};
+
+//2. addEventListener and check email & password using function checkLog
+btnLog.addEventListener("click", checkLog);
--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
-/** @format */
+// button calculez IMC
+let btnImc = document.getElementById("btnImc");
+let displayResultat = document.querySelector(".resultat");
 
 // Animation accueil
-var bgImageArray = ["1", "2", "3", "4", "5", "6"],
-  base = "../res/Accueil/bg",
+const bgImageArray = ["1", "2", "3", "4", "5", "6"],
+  base = "./res/Accueil/bg",
   secs = 4;
-bgImageArray.forEach(function (img) {
+bgImageArray.forEach((img) => {
   new Image().src = base + img + ".jpg";
   // caches images, avoiding white flash between background replacements
 });
 
-function backgroundSequence() {
+const backgroundSequence = () => {
   window.clearTimeout();
-  var k = 0;
-  for (i = 0; i < bgImageArray.length; i++) {
+  let k = 0;
+  for (let i = 0; i < bgImageArray.length; i++) {
     setTimeout(function () {
       document.getElementById("animated-bg").style.background =
         "url(" + base + bgImageArray[k] + ".jpg" + ") no-repeat ";
@@ -26,5 +28,18 @@ function backgroundSequence() {
       }
     }, secs * 1000 * i);
   }
-}
+};
 backgroundSequence();
+
+// Calcul IMC
+const CalculImc = (e) => {
+  e.preventDefault();
+  const poids = document.getElementById("poids").innerHTML;
+  console.log(poids);
+  // const imc = (poids * 10000) / (taille * taille);
+  // alert(imc);
+  // imc = Math.round(imc);
+};
+//addEventListener and display result using function calcul
+btnImc.addEventListner("click", (e) => calculeImc(e));
+// btnImc.addEventListener("click", calculImc);
